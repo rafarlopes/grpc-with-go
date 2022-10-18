5b9ccd5573e3a1aab830f334286d42cc261a33f2
Rafael Romero Lopes <rafael.romero.lopes@ingrid.com>
2022-10-18 15:44:24 +0200

    added backoff to retry failed messages

--- streams-client/main.go
+++ streams-client/main.go
@@ -2,49 +2,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"log"
+	"sync"
 	"time"
 
+	backoff "github.com/cenkalti/backoff/v4"
 	"github.com/rafarlopes/grpc-with-go/streams"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer conn.Close()
-
-	c := streams.NewGreeterClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
-
-	stream, err := c.SayHello(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	wait := make(chan interface{})
-
-	go func() {
-		for {
-			resp, err := stream.Recv()
-			if err == io.EOF {
-				close(wait)
-				return
-			}
-			if err != nil {
-				log.Fatalf("\nmessage failed %v", err)
-			}
-
-			log.Printf("\ngot message %s", resp.GetMessage())
-		}
-	}()
+	mu := &sync.Mutex{}
 
 	messages := []*streams.HelloRequest{
 		{Name: "aaa"},
@@ -53,13 +24,59 @@ func main() {
 		{Name: "ddd"},
 	}
 
-	for _, msg := range messages {
-		if err := stream.Send(msg); err != nil {
-			log.Fatalf("\nfailed to send message %v", err)
+	backoff.Retry(func() error {
+		log.Println("starting...")
+		conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			return err
 		}
-	}
 
-	stream.CloseSend()
+		defer conn.Close()
 
-	<-wait
+		c := streams.NewGreeterClient(conn)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+		defer cancel()
+
+		stream, err := c.SayHello(ctx)
+		if err != nil {
+			return err
+		}
+
+		wait := make(chan interface{})
+
+		go func() {
+			for {
+				resp, err := stream.Recv()
+				if err == io.EOF {
+					close(wait)
+					return
+				}
+				if err != nil {
+					log.Printf("\nmessage failed %v", err)
+					return
+				}
+
+				log.Printf("\ngot message %s", resp.GetMessage())
+				mu.Lock()
+				messages = messages[1:]
+				mu.Unlock()
+			}
+		}()
+
+		for _, msg := range messages {
+			if err := stream.Send(msg); err != nil {
+				return fmt.Errorf("\nfailed to send message %v", err)
+			}
+
+			time.Sleep(10 * time.Second)
+		}
+
+		stream.CloseSend()
+
+		<-wait
+
+		return nil
+
+	}, backoff.NewConstantBackOff(5*time.Second))
 }
